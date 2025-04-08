082e16ae11fa0c301cb98c945b8f4a5af9ffffcf
Matteo <72200721+Matteo-Spina@users.noreply.github.com>
2025-04-09 01:16:33 +0200

    revert create_database.php and config.php, then reapply only the most significant changes.

--- database/create_database.php
+++ database/create_database.php
@@ -1,30 +1,12 @@
-<?php
-
-// This script is for developers who wish to connect to their own MariaDB
-// instance to create a new db 'piante'.
-
-// Temp instructions:
-// Mariadb instance connection parameters using a Unix socket and socket authentication
-$dsn = 'mysql:unix_socket=/run/mysqld/mysqld.sock;';
-$username = 'matteo'; // Should match your system user
-$password = ''; // No password needed with socket-based authentication
-
+<?php // Creates the database
 try {
-    echo "Trying to connect \n";
-    // Connecting to the database, returns a ref to PDO instance
-    $pdo = new PDO($dsn, $username, $password);
-    // Set error mode to Exception so that errors throw exceptions
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connection successful, \n";
-
-    echo "Creating database 'piante' \n";
+    require '../public/api/config.php'; // use config in public folder
+    $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
     $sql = file_get_contents('database.sql');
+    $sql = str_replace('`piante`', DB_NAME, $sql); // use your db name defined in config
     $pdo->exec($sql);
-    // echo 'Complimenti!<br>Hai creato il database <code>' . DB_NAME . '</code>.';
-    echo "Database 'piante' created \n";
-} catch (PDOException $exception) {
-    // If the connection fails, output the error message
-    echo "Error " . $exception->getMessage();
+    echo 'Complimenti!<br>Hai creato il database <code>' . DB_NAME . '</code>.';
+} catch (Exception $exception) {
+    echo $exception->getMessage();
 }
-
-// $pdo = null; // Close the connection (optional)
+?>
