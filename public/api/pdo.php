3c1d7972f925ee8dcaa1565b0f109a5ac51cf4f4
Matteo <matteo.spina@hotmail.com>
2025-04-08 11:45:55 +0200

    add comments to php-mariadb configs

--- public/api/pdo.php
+++ public/api/pdo.php
@@ -1,4 +1,8 @@
 <?php
+
+// This should match either your development environment or the production
+// environment
+
 require_once '../config.php';
 $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- public/config.php
+++ public/config.php
@@ -1,7 +1,10 @@
 <?php
+
+// This should match either your development environment or the production
+// environment
+
 define('DB_HOST', 'localhost');
 define('DB_USER', '');
 define('DB_PASSWORD', '');
-
 define('DB_NAME', 'piante');
 ?>
