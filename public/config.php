2e9759abfbd123beeeb99eab0ad2fee127889564
Matteo <matteo.spina@hotmail.com>
2025-04-08 18:12:08 +0200

    change connection configs

--- public/config.php
+++ public/config.php
@@ -3,7 +3,7 @@
 // This should match either your development environment or the production
 // environment
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql:host="localhost"');
 define('DB_USER', '');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'piante');
