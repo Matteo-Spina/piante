e1e129d3292d92d60cb087449e69fc7b7ac7c767
Matteo <72200721+Matteo-Spina@users.noreply.github.com>
2025-04-09 09:12:52 +0200

    revert pdo.php, change DB_HOST string

--- public/api/pdo.php
+++ public/api/pdo.php
@@ -1,17 +1,6 @@
 <?php
-// For now, using hard coded values
-// require_once '../config.php';
-
-// Values should match either your development environment or the production
-// environment
-
-// Database connection parameters using a Unix socket and socket authentication
-$dsn = 'mysql:unix_socket=/run/mysqld/mysqld.sock;dbname=piante;charset=utf8mb4';
-$username = 'matteo'; // Should match your system user
-$password = ''; // No password needed with socket-based authentication
-
-// $pdo = new PDO(DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
-$pdo = new PDO($dsn, $username, $password);
-
+require_once '../config.php';
+$pdo = new PDO(DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+?>
